820373451261e23e27feff00a469e9bf726559e6
Tim O'Brien <tim@tekao.net>
2014-08-12 18:18:35 +0100

    add java example methods for new coversions

--- tests/java_example/src/local/Foo.java
+++ tests/java_example/src/local/Foo.java
@@ -1,7 +1,7 @@
 package local;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.InetAddress;
+import java.util.*;
 
 public class Foo {
     public Foo(boolean bad) throws Exception {
@@ -29,6 +29,55 @@ public class Foo {
         for (int i = 0; i < words.length; i++) {
             ret += words[i] + " ";
         }
+
+        return ret;
+    }
+
+    public Set<String> Method4(Collection<String> x) {
+        HashSet<String> y =  new HashSet<String>();
+
+        Iterator<String> iter = x.iterator();
+        while (iter.hasNext()) {
+            y.add(iter.next());
+        }
+        y.add("end");
+
+        return y;
+    }
+
+    public Map<String, String> Method5(Map<String, Integer> x) {
+        HashMap<String, String> ret = new HashMap<String, String>();
+
+        List<String> k = new ArrayList<String>(x.keySet());
+        for (int i = 0; i < k.size(); i++) {
+            String str;
+            str = x.get(k.get(i)) + "x";
+            ret.put(k.get(i), str);
+        }
         return ret;
     }
 }
+
+/*
+        Set<String> z = new HashSet<String>();
+
+        z.add("hello");
+
+        Collection<String> x = new ArrayList<String>();
+
+        Iterator<String> w = x.iterator();
+//        z.
+
+
+//        y = x.iterator();
+
+        Map<String, String> m = new HashMap<String, String>();
+        m.put("a", "b");
+        m.keySet()
+
+        Map.Entry<String, String> g;
+        sfsd = new HashMap<String, String>()
+
+        InetAddress v;
+        v.get
+*/
